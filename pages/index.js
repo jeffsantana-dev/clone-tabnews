c83b74d22162387060b9125d62c9e47892fa69b4
jeffersonssantana <jeffersons.tst@icloud.com>
2026-01-03 00:22:40 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Vitória, eu amo voce. Se voce me ama, de uma risadinha</h1>
+    return <h1>Entre bilhóes de pessoas, meu coração realmente escolheu voce para ser o amor dele 💚🤍🖤</h1>
 }
 
 export default Home;
