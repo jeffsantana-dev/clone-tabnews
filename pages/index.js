ce85071f4329288eb230c8260ef382d0eb0ca7d6
jeffersonssantana <jeffersons.tst@icloud.com>
2026-01-04 13:54:55 +0000

    modificar arquivo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Entre bilhões de pessoas, meu coração realmente escolheu você para ser o amor dele 💚🤍🖤</h1>
+    return <h1>Meu primeiro projeto foi pensando em você. Dedicado a você. E através dele, quero dizer que te amo muito, sinto muito sua falta e que voce ainda é O AMOR DA MINHA VIDA. 🎶As vezes eu acordo assim com um furacão dentro de mim. Perdido e carente de uma ligação, querendo uma resposta. Impossivel não lembrar do nosso amor, da trajetória. Tatuou meu coração, em minha vida fez história.🎶 💚🤍🖤 </h1>
 }
 
 export default Home;
